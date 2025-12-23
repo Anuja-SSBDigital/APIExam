21f15966ab2016dbb04ed7af048a2ab83992c8d4
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-12-23 16:28:02 +0530

    Add Dto

---
+++ Model/DTOs/StudentRegisteredListdto.cs
@@ -0,0 +1,15 @@
+﻿namespace APIExam.Model.DTOs
+#pragma warning disable
+{
+    public class StudentRegisteredListdto
+    {
+        public string CollegeCode  { get; set; }
+        public string StudentName { get; set; }
+
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+
+        public string YearOfPassing { get; set; }
+      
+    }
+}
---
+++ Model/DTOs/StudentRegisteredListsqlRow.cs
@@ -0,0 +1,26 @@
+﻿namespace APIExam.Model.DTOs
+{
+    public class StudentRegisteredListsqlRow
+    {
+
+        public string CollegeCode { get; set; }
+
+        public string StudentName { get; set; }
+
+        public bool IsRegFeeSubmit { get; set; }
+
+        public bool IsRegFormSubmit { get; set; }
+
+        public string FatherName { get; set; }
+
+        public string MotherName { get; set; }
+
+        public string YearOfPassing { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string Faculty { get; set; }
+
+        public int StudentID { get; set; }
+    }
+}
