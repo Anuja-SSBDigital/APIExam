65646f851f0399043072e690000642e7cdb56c32
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-12-23 16:26:33 +0530

    Add Service

---
+++ Services/IServices/IStudentRegisteredListService.cs
@@ -0,0 +1,13 @@
+﻿using APIExam.Model.DTOs;
+
+namespace APIExam.Services.IServices
+{
+    public interface IStudentRegisteredListService
+    {
+        Task<List<StudentRegisteredListdto>> GetStudentRegisteredListAsync(
+           int collegeId,
+           int facultyId,
+           int examId
+          );
+    }
+}
---
+++ Services/Services/StudentRegisteredListService.cs
@@ -0,0 +1,38 @@
+﻿using APIExam.Model.DTOs;
+using APIExam.Repositories;
+using APIExam.Repositories.IRepositories;
+using APIExam.Services.IServices;
+
+namespace APIExam.Services.Services      
+{
+    public class StudentRegisteredListService : IStudentRegisteredListService
+    {
+        private readonly IStudentRegisteredListRepository _repository;       
+
+        public StudentRegisteredListService(IStudentRegisteredListRepository repository)
+        {
+            _repository = repository;      
+        }
+
+       
+        public async Task<List<StudentRegisteredListdto>> GetStudentRegisteredListAsync(
+            int collegeId,
+            int facultyId,
+            int examId
+        )                      
+        {
+            var rawData = await _repository.GetExamStudentRawListAsync(
+                collegeId, facultyId, examId);      
+            
+           
+            return rawData.Select(item => new StudentRegisteredListdto
+            {
+                CollegeCode = item.CollegeCode ?? "",
+                StudentName = item.StudentName,
+                FatherName = item.FatherName,
+                MotherName = item.MotherName,
+                YearOfPassing = item.YearOfPassing
+            }).ToList();
+        }
+    }
+}
