89639b624e597acc464ef13e92b989f9845aa09b
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-12-23 16:27:15 +0530

    StudentRegisteredList

---
+++ Repositories/IStudentRegisteredListRepository.cs
@@ -0,0 +1,16 @@
+﻿using APIExam.Model.DTOs;
+
+namespace APIExam.Repositories
+{
+    public interface IStudentRegisteredListRepository
+    {
+
+       
+        Task<List<StudentRegisteredListsqlRow>> GetExamStudentRawListAsync(
+           int collegeId,
+           int facultyId,
+           int examId
+       );
+    }
+}
+
---
+++ Repositories/StudentRegisteredListRepository.cs
@@ -0,0 +1,35 @@
+﻿using APIExam.Data;
+using APIExam.Model.DTOs;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIExam.Repositories
+{
+    public class StudentRegisteredListRepository : IStudentRegisteredListRepository
+    {
+        private readonly AppDbContext _context;
+
+        public StudentRegisteredListRepository(AppDbContext context)
+        {
+            _context = context;
+        }   
+           
+        
+        public async Task<List<StudentRegisteredListsqlRow>> GetExamStudentRawListAsync(
+            int collegeId,
+            int facultyId,
+            int examId
+        )
+        {
+            return await _context.StudentRegisteredListsqlRow
+                .FromSqlRaw(
+                    "EXEC sp_GetStudentExaminationListData @CollegeId, @FacultyId, @ExamId",
+                    new SqlParameter("@CollegeId", collegeId),
+                    new SqlParameter("@FacultyId", facultyId),
+                    new SqlParameter("@ExamId", examId)
+                )
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
