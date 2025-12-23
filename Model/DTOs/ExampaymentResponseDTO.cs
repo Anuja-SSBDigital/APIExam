c4a405b1df1a9f7bb14dc6cc70073262877c5ff7
Anuja Dubey <anuja.dubey@ssbi.in>
2025-12-23 14:46:16 +0530

    Exam Payment Response DTO

---
+++ Model/DTOs/ExampaymentResponseDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace APIExam.Model.DTOs
+{
+    public class ExampaymentResponseDTO
+    {
+        public int StudentID { get; set; }
+        public int RegistrationNo { get; set; }
+        public int Name { get; set; }
+        public int FatherName { get; set; }
+        public int MotherName { get; set; }
+        public int DOB { get; set; }
+        public int CategoryName { get; set; }
+        public int Faculty { get; set; }
+        public int ExamTypeName { get; set; }
+        public int ExamTypeId { get; set; }
+        public int College { get; set; }
+        public int OfssReferenceNo { get; set; }
+        public int CollegeCode { get; set; }
+        public int FormDownloaded { get; set; }
+        public int ConcessionFee { get; set; }
+        public int BaseFee { get; set; }
+        public int CasteCategoryCode { get; set; }
+        public int IsFirstExam { get; set; }
+       
+    }
+}
