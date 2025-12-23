15b597d93ad874e68ccb1516a9200abfdf338797
Anuja Dubey <anuja.dubey@ssbi.in>
2025-12-23 16:13:22 +0530

    Exam Payment List

---
+++ Controllers/ExamPaymentController.cs
@@ -0,0 +1,36 @@
+﻿using APIExam.Model;
+using APIExam.Services.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIExam.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamPaymentController : ControllerBase
+    {
+
+        private readonly IExamPaymentService _examPaymentService;
+
+        public ExamPaymentController(IExamPaymentService examPaymentService)
+        {
+            _examPaymentService = examPaymentService;
+        }
+
+        [HttpPost("ExamPayment")]
+        public async Task<IActionResult> ExamPayment([FromForm] DonwloadExamformModel examformModel)
+        {
+            try
+            {
+                var result = await _examPaymentService.ExamPaymentAsync(examformModel);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (you can use any logging framework)
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
+        }
+
+    }
+}
--- Model/DTOs/ExampaymentResponseDTO.cs
+++ Model/DTOs/ExampaymentResponseDTO.cs
@@ -3,23 +3,23 @@
     public class ExampaymentResponseDTO
     {
         public int StudentID { get; set; }
-        public int RegistrationNo { get; set; }
-        public int Name { get; set; }
-        public int FatherName { get; set; }
-        public int MotherName { get; set; }
-        public int DOB { get; set; }
-        public int CategoryName { get; set; }
-        public int Faculty { get; set; }
-        public int ExamTypeName { get; set; }
-        public int ExamTypeId { get; set; }
-        public int College { get; set; }
-        public int OfssReferenceNo { get; set; }
-        public int CollegeCode { get; set; }
-        public int FormDownloaded { get; set; }
-        public int ConcessionFee { get; set; }
-        public int BaseFee { get; set; }
-        public int CasteCategoryCode { get; set; }
-        public int IsFirstExam { get; set; }
+        public string? RegistrationNo { get; set; }
+        public string? Name { get; set; }
+        public string? FatherName { get; set; }
+        public string? MotherName { get; set; }
+        public DateTime? DOB { get; set; }
+        public string? CategoryName { get; set; }
+        public string? Faculty { get; set; }
+        public string? ExamTypeName { get; set; }
+        public int? ExamTypeId { get; set; }
+        public string? College { get; set; }
+        public string? OfssReferenceNo { get; set; }
+        public string? CollegeCode { get; set; }
+        public bool? FormDownloaded { get; set; }
+        public int? ConcessionFee { get; set; }
+        public int? BaseFee { get; set; }
+        public string? CasteCategoryCode { get; set; }
+        public bool? IsFirstExam { get; set; }
        
     }
 }
