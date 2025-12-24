1ccfbc6bdfa7653dec74ce63b0284957c3bb4879
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-12-24 16:08:31 +0530

    Add Service

---
+++ Services/Services/ExamPaymentStatusService.cs
@@ -0,0 +1,26 @@
+﻿using APIExam.Model.DTOs;
+using APIExam.Repositories;
+using APIExam.Services.IServices;
+
+namespace APIExam.Services.Services
+{
+    public class ExamPaymentStatusService : IExamPaymentStatusService
+    {
+        private readonly IExamPaymentStatusRepository _repository;
+
+        public ExamPaymentStatusService(IExamPaymentStatusRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<ExamPaymentStatusDTO>> GetPaymentDetailsAsync(
+            int collegeId,
+            string? collegeCode,
+            int examId,
+            string? clientTxnId)
+        {
+            return await _repository.GetPaymentDetailsAsync(
+                collegeId, collegeCode, examId, clientTxnId);
+        }
+    }
+}
---
+++ Services/Services/PaymentSummaryService.cs
@@ -0,0 +1,21 @@
+﻿using APIExam.Model.DTOs;
+using APIExam.Repositories;
+using APIExam.Services.IServices;
+
+namespace APIExam.Services.Services
+{
+    public class PaymentSummaryService : IPaymentSummaryService
+    {
+        private readonly IPaymentSummaryRepository _repository;
+
+        public PaymentSummaryService(IPaymentSummaryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<ExamFeeFormSummaryDTO>> GetSummaryAsync(int collegeId)
+        {
+            return await _repository.GetSummaryAsync(collegeId);
+        }
+    }
+}
