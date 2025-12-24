4e80518c025159fd6ab07dc609c6cb10dd321f08
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-12-24 16:12:31 +0530

    Add Api

---
+++ Controllers/PaymentSummaryController.cs
@@ -0,0 +1,29 @@
+﻿using APIExam.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIExam.Controllers
+{
+   
+        [ApiController]
+        [Route("api/[controller]")]
+        public class PaymentSummaryController : ControllerBase
+        {
+            private readonly IPaymentSummaryService _service;
+
+            public PaymentSummaryController(IPaymentSummaryService service)
+            {
+                _service = service;
+            }
+
+            [HttpGet("CollegeWise")]
+            public async Task<IActionResult> GetCollegeWiseSummary(int collegeId)
+            {
+                var data = await _service.GetSummaryAsync(collegeId);
+
+                if (data == null || data.Count == 0)
+                    return NotFound("No summary data found");
+
+                return Ok(data);
+            }
+        }
+    }
