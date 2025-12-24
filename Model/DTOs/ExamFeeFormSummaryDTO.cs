16538f693c345333240091e776ca14e1aa978903
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-12-24 16:12:08 +0530

    Add Model

---
+++ Model/DTOs/ExamFeeFormSummaryDTO.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIExam.Model.DTOs
+{
+    [Keyless]
+    public class ExamFeeFormSummaryDTO
+    {
+       
+
+        [Column("Faculty")]
+        public string? Faculty { get; set; }
+
+       
+        [Column("Exam Type")]
+        public string? ExamType { get; set; }
+
+        
+        [Column("Fee Submitted")]
+        public int FeeSubmitted { get; set; }
+
+        
+        [Column("Form Submitted")]
+        public int FormSubmitted { get; set; }
+
+
+        [Column("Form Not Submitted")]
+        public int FormNotSubmitted { get; set; }
+    }
+}
---
+++ Services/IServices/IPaymentSummaryService.cs
@@ -0,0 +1,9 @@
+﻿using APIExam.Model.DTOs;
+
+namespace APIExam.Services.IServices
+{
+    public interface IPaymentSummaryService
+    {
+        Task<List<ExamFeeFormSummaryDTO>> GetSummaryAsync(int collegeId);
+    }
+}
