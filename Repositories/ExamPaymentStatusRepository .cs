d8d167d32dc8dbf1e3b257130c98b26b8693f624
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-12-26 17:43:24 +0530

    Add Payment status

---
+++ Repositories/ExamPaymentStatusRepository .cs
@@ -0,0 +1,38 @@
+﻿using APIExam.Data;
+using APIExam.Model.DTOs;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIExam.Repositories
+{
+    public class ExamPaymentStatusRepository : IExamPaymentStatusRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ExamPaymentStatusRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ExamPaymentStatusDTO>> GetPaymentDetailsAsync(
+            int collegeId,
+            string? collegeCode,
+            int examId,
+            string? clientTxnId)
+        {
+            var parameters = new[]
+            {
+                new SqlParameter("@CollegeId", collegeId),
+                new SqlParameter("@CollegeCode", (object?)collegeCode ?? DBNull.Value),
+                new SqlParameter("@ClientTxnId", (object?)clientTxnId ?? DBNull.Value),
+                new SqlParameter("@ExamId", examId)
+            };
+
+            return await _context.ExamPaymentStatusDTOs
+                .FromSqlRaw(
+                    "EXEC dbo.sp_GetExamPaymentDetails @CollegeId, @CollegeCode, @ClientTxnId, @ExamId",
+                    parameters)
+                .ToListAsync();
+        }
+    }
+}
---
+++ Repositories/IExamPaymentStatusRepository.cs
@@ -0,0 +1,14 @@
+﻿using APIExam.Model.DTOs;
+
+namespace APIExam.Repositories
+{
+    public interface IExamPaymentStatusRepository
+    {
+        Task<List<ExamPaymentStatusDTO>> GetPaymentDetailsAsync(
+            int collegeId,
+            string? collegeCode,
+            int examId,
+            string? clientTxnId
+        );
+    }
+}
