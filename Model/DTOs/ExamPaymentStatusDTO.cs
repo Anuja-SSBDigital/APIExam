c86a651adb5f652fb00941a43ed5782785a3bc74
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-12-26 17:41:41 +0530

    add service

---
+++ Model/DTOs/ExamPaymentStatusDTO.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIExam.Model.DTOs
+{
+    [Keyless]
+    public class ExamPaymentStatusDTO
+    {
+        public int Pk_PaymentId { get; set; }
+        public int Fk_CollegeId { get; set; }
+        public int Fk_FeeTypeId { get; set; }
+
+        public string? BankGateway { get; set; }
+        public string? PaymentMode { get; set; }
+
+        public decimal? PaymentAmount { get; set; }
+        public decimal? AmountPaid { get; set; }
+
+        public string? ClientTxnId { get; set; }
+        public string? GatewayTxnId { get; set; }
+        public string? BankTxnId { get; set; }
+
+        public string? PaymentStatus { get; set; }
+        public string? PaymentStatusCode { get; set; }
+
+        public DateTime? PaymentInitiateDate { get; set; }
+        public DateTime? PaymentUpdatedDate { get; set; }
+        public DateTime? PaymentCreatedOn { get; set; }
+
+        public int? StudentsPerTransaction { get; set; }
+
+        public string? CollegeName { get; set; }
+        public string? CollegeCode { get; set; }
+    }
+}
---
+++ Services/IServices/IExamPaymentStatusService.cs
@@ -0,0 +1,14 @@
+﻿using APIExam.Model.DTOs;
+
+namespace APIExam.Services.IServices
+{
+    public interface IExamPaymentStatusService
+    {
+        Task<List<ExamPaymentStatusDTO>> GetPaymentDetailsAsync(
+            int collegeId,
+            string? collegeCode,
+            int examId,
+            string? clientTxnId
+        );
+    }
+}
