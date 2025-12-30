2dc511812e45f7787a57480ab9dcbe569d955dc3
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-12-30 17:49:18 +0530

    Add Api

---
+++ Controllers/TheoryAdmitCardController.cs
@@ -0,0 +1,32 @@
+﻿using APIExam.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIExam.Controllers
+{                  
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TheoryAdmitCardController : ControllerBase
+    {
+        private readonly ITheoryAdmitCardService _service;
+
+       
+        public TheoryAdmitCardController(ITheoryAdmitCardService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> GetTheoryAdmitCard(
+            int collegeId, int facultyId, int examId)
+        {
+            var data = await _service.GetTheoryAdmitCardAsync(
+                collegeId, facultyId, examId);
+
+            if (data == null || data.Count == 0)
+                return NotFound("No theory admit card data found");
+
+            return Ok(data);
+        }
+    }
+}
+
