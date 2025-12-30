6986324a5e4788c27d150b2e273fac4137be8b7b
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-12-30 17:47:59 +0530

    Add Service

---
+++ Services/Services/TheoryAdmitCardService.cs
@@ -0,0 +1,23 @@
+﻿using APIExam.Model.DTOs;
+using APIExam.Repositories;
+using APIExam.Services.IServices;
+
+namespace APIExam.Services.Services
+{
+    public class TheoryAdmitCardService : ITheoryAdmitCardService
+    {
+        private readonly ITheoryAdmitCardRepository _repository;
+
+        public TheoryAdmitCardService(ITheoryAdmitCardRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<TheoryAdmitCardDTO>> GetTheoryAdmitCardAsync(
+            int collegeId, int facultyId, int examId)
+        {
+            return await _repository.GetTheoryAdmitCardAsync(
+                collegeId, facultyId, examId);
+        }
+    }
+}
