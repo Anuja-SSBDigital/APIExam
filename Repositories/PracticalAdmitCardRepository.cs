8de8e687cfecf657561f73d839b10b05b95c0b7d
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2025-12-31 17:52:42 +0530

    Add Repository

---
+++ Repositories/PracticalAdmitCardRepository.cs
@@ -0,0 +1,32 @@
+﻿using APIExam.Data;
+using APIExam.Model.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIExam.Repositories
+{
+    public class PracticalAdmitCardRepository : IPracticalAdmitCardRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PracticalAdmitCardRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<PracticalAdmitCardDTO>> GetPracticalAdmitCardAsync(
+            int collegeId, int facultyId, int examId)
+        {
+            return await _context.PracticalAdmitCards
+                .FromSqlRaw(
+                    @"EXEC sp_GetExamDummyAdmitCardData
+                        @Fk_CollegeId={0},
+                        @Fk_FacultyId={1},
+                        @ExamId={2},
+                        @IsPractical=1,
+                        @IsTheory=NULL",
+                    collegeId, facultyId, examId)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
