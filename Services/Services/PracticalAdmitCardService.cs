00f24445405381526a656c3a4203dc1da7f8d1c3
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2026-01-01 17:40:01 +0530

    Add Service

---
+++ Services/Services/PracticalAdmitCardService.cs
@@ -0,0 +1,23 @@
+﻿using APIExam.Model.DTOs;
+using APIExam.Repositories;
+using APIExam.Services.IServices;
+
+namespace APIExam.Services.Services
+{
+    public class PracticalAdmitCardService : IPracticalAdmitCardService
+    {
+        private readonly IPracticalAdmitCardRepository _repository;
+
+        public PracticalAdmitCardService(IPracticalAdmitCardRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<PracticalAdmitCardDTO>> GetPracticalAdmitCardAsync(
+            int collegeId, int facultyId, int examId)
+        {
+            return await _repository.GetPracticalAdmitCardAsync(
+                collegeId, facultyId, examId);
+        }
+    }
+}
