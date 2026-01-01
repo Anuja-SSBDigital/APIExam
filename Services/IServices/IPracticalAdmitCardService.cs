9af2d72006b1ca3fb936328e85b5b4660fc81b40
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2026-01-01 17:34:55 +0530

    Add Service

---
+++ Services/IServices/IPracticalAdmitCardService.cs
@@ -0,0 +1,10 @@
+﻿using APIExam.Model.DTOs;
+
+namespace APIExam.Services.IServices
+{
+    public interface IPracticalAdmitCardService
+    {
+        Task<List<PracticalAdmitCardDTO>> GetPracticalAdmitCardAsync(
+            int collegeId, int facultyId, int examId);
+    }
+}
