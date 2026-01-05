9375f5f74aaecf5e64e8c2f4b92b022ee68c5f43
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2026-01-05 11:06:49 +0530

    changes

---
+++ Services/Services/DummyExamAdmitcardService.cs
@@ -0,0 +1,26 @@
+﻿using APIExam.Model.DTOs;
+using APIExam.Repositories;
+using APIExam.Services.IServices;
+
+namespace APIExam.Services.Services
+{
+    public class DummyExamAdmitcardService:IDummyExamAdmitcardService
+    {
+        private readonly IDummyExamAdmitcardRepository _repository;
+
+
+        public DummyExamAdmitcardService(IDummyExamAdmitcardRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+
+        public async Task<List<DummyExamAdmitcarddto>> GetdummyAdmitCardAsync(
+            int collegeId, int facultyId, int examId)
+        {
+            return await _repository.GetdummyAdmitCardAsync(
+                collegeId, facultyId, examId);
+        }
+    }
+}
