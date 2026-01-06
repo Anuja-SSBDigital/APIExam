a656c865d027cba9fbbdb6b82abcd7543da7b762
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2026-01-06 18:03:12 +0530

    add repositories

---
+++ Repositories/DummyExamAdmitcardRepository.cs
@@ -0,0 +1,33 @@
+﻿using APIExam.Data;
+using APIExam.Model.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIExam.Repositories
+{
+    public class DummyExamAdmitcardRepository:IDummyExamAdmitcardRepository
+    {
+        private readonly AppDbContext _context;
+
+        public DummyExamAdmitcardRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+    
+        public async Task<List<DummyExamAdmitcarddto>> GetdummyAdmitCardAsync(
+            int collegeId, int facultyId, int examId)
+        {
+            return await _context.DummyExamAdmitcarddto
+                .FromSqlRaw(
+                    @"EXEC sp_GetExamDummyAdmitCardData 
+                        @Fk_CollegeId={0},
+                        @Fk_FacultyId={1},
+                        @ExamId={2},
+                        @IsPractical=NULL,
+                        @IsTheory=1",
+                    collegeId, facultyId, examId)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
