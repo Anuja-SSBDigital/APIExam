b2245c32509fc3ad9ee131cb2db243ab00582ce1
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2026-01-06 18:01:42 +0530

    add code

---
+++ Repositories/IDummyExamAdmitcardRepository.cs
@@ -0,0 +1,11 @@
+﻿using APIExam.Model.DTOs;
+
+namespace APIExam.Repositories
+{
+    public interface IDummyExamAdmitcardRepository
+    {
+
+        Task<List<DummyExamAdmitcarddto>> GetdummyAdmitCardAsync(
+         int collegeId, int facultyId, int examId);
+    }
+}
