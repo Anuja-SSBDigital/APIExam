a6ab89d61d62f25e4069e0a57e15da30463c788c
ShubhanshuMishra <shubanshu.mishra@ssbi.in>
2026-01-06 18:01:26 +0530

    add interface

---
+++ Services/IServices/IDummyExamAdmitcardService.cs
@@ -0,0 +1,10 @@
+﻿using APIExam.Model.DTOs;
+
+namespace APIExam.Services.IServices
+{
+    public interface IDummyExamAdmitcardService
+    {
+        Task<List<DummyExamAdmitcarddto>> GetdummyAdmitCardAsync(
+         int collegeId, int facultyId, int examId);
+    }
+}
